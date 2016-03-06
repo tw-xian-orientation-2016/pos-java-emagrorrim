1762c73878b78650386cd1fc75290024d13e3212
emagrorrom <373407641@qq.com>
2016-03-06 19:44:37 +0800

    all task work

--- src/Pos.java
+++ src/Pos.java
@@ -1,9 +1,62 @@
-/**
- * Created by apple on 16/3/4.
- */
+import java.util.ArrayList;
+
 public class Pos {
-//
-//    public CartItems[] getCartItems(String[] tags, Item[] allItems) {
-//
-//    }
+
+    public CartItem[] getCartItems(String[] tags, Item[] allItems) {
+
+        ArrayList<CartItem> cartItems = new ArrayList<>();
+
+        for (int i = 0; i < tags.length; i++) {
+            String[] tagComponents = tags[i].split("-");
+            String barcode = tagComponents[0];
+            double count = tagComponents.length > 1 ? Double.parseDouble(tagComponents[1]) : 1;
+
+            CartItem cartItem = findCartItem(barcode, cartItems);
+            if (cartItem != null) {
+                cartItem.count += count;
+            } else {
+                cartItems.add(new CartItem(barcode, count, allItems));
+            }
+        }
+
+        return cartItems.toArray(new  CartItem[cartItems.size()]);
+    }
+
+
+    private CartItem findCartItem(String barcode, ArrayList<CartItem> cartItems) {
+
+        for (int i = 0; i < cartItems.size(); i++) {
+            CartItem cartItem = cartItems.get(i);
+            if (barcode.equals(cartItem.getBarcode())) {
+                return cartItem;
+            }
+        }
+
+        return null;
+    }
+
+    public ReceiptItem[] getReceiptItems(CartItem[] cartItems) {
+
+        ArrayList<ReceiptItem> receiptItems = new ArrayList<>();
+
+        for (int i = 0; i < cartItems.length; i++) {
+
+            ReceiptItem receiptItem = new ReceiptItem(cartItems[i]);
+            receiptItems .add(receiptItem);
+        }
+
+        return receiptItems.toArray(new ReceiptItem[receiptItems.size()]);
+    }
+
+    public Receipt getReceipt(ReceiptItem[] receiptItems) {
+        return new Receipt(receiptItems);
+    }
+
+
+    public void printReceipt(String receiptText) {
+
+        System.out.println(receiptText);
+    }
+
+
 }
